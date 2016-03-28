e688d2b77c5581996d8152bc062f24f57e5bd262
Mordi Sacks <mordisacks@gmail.com>
2016-03-29 02:18:58 +0300

    forgot actual migration file..

---
+++ database/migrations/2016_03_28_224415_create_keys_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKeysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keys', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('email');
+            $table->text('key');
+            $table->string('token');
+            $table->boolean('compromised')->default(false);
+            $table->boolean('verified')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('keys');
+    }
+}
